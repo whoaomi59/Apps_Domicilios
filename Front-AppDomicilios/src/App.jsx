2b26085cf952ca9abd07c08efe8c9535046570af
whoaomi59 <whoaomi11@gmail.com>
2025-05-21 13:52:37 -0500

    nuevos cambios en productos

--- Front-AppDomicilios/src/App.jsx
+++ Front-AppDomicilios/src/App.jsx
@@ -29,7 +29,6 @@ import Baner_Negocios from "./view/admin/bener_negocios";
 import RegisterClient from "./view/client/Register";
 import Categorias_Negocios from "./view/admin/Categoria_Negocio";
 
-/* export const URL = "https://domicilios.fundacionhuellas.com.co/API/"; */
 export const URL = "https://runway.com.co/API/";
 // export const URL = " http://localhost/Apps_Domicilios/API/";
 
--- Front-AppDomicilios/src/view/Shop/Productos/index.jsx
+++ Front-AppDomicilios/src/view/Shop/Productos/index.jsx
@@ -142,7 +142,6 @@ export default function ProductosShop() {
                     <p class="text-lg font-semibold leading-tight text-gray-900 hover:underline">
                       {item.Producto}
                     </p>
-
                     <div class="mt-2 flex items-center gap-2">
                       <div class="flex items-center">
                         <svg
@@ -200,49 +199,17 @@ export default function ProductosShop() {
                         5.0
                       </p>
                     </div>
-
                     <ul class="mt-2 flex items-center gap-4">
-                      <li class="flex items-center gap-2">
-                        <svg
-                          class="h-4 w-4 text-gray-500 .:text-gray-400"
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                        >
-                          <path
-                            stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M13 7h6l2 4m-8-4v8m0-8V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v9h2m8 0H9m4 0h2m4 0h2v-4m0 0h-5m3.5 5.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Zm-10 0a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z"
-                          />
-                        </svg>
+                      <li class="flex items-center gap-1">
+                        <span>🍛</span>
                         <p class="text-sm font-medium text-gray-500 .:text-gray-400">
                           {item.Tipo}
                         </p>
                       </li>
-
-                      <li class="flex items-center gap-2">
-                        <svg
-                          class="h-4 w-4 text-gray-500 .:text-gray-400"
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                        >
-                          <path
-                            stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-width="2"
-                            d="M8 7V6c0-.6.4-1 1-1h11c.6 0 1 .4 1 1v7c0 .6-.4 1-1 1h-1M3 18v-7c0-.6.4-1 1-1h11c.6 0 1 .4 1 1v7c0 .6-.4 1-1 1H4a1 1 0 0 1-1-1Zm8-3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"
-                          />
-                        </svg>
-                        <p class="text-sm font-medium text-gray-500 .:text-gray-400">
-                          Mejor precio
-                        </p>
-                      </li>
-                    </ul>
+                    </ul>{" "}
+                    <p class="text-sm font-medium text-gray-500 .:text-gray-400">
+                      ​{item.descripcion}
+                    </p>
                     <p class="text-2xl font-extrabold leading-tight text-gray-900">
                       {formatearCOP(item.precio)}
                     </p>
